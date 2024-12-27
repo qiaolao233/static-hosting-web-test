579444d20e973e4da50b86776e8f7a538b8d43bd
joejoewang <joejoewang@tungee.com>
2024-12-27 17:15:40 +0800

    feat: 添加 pinia

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { useCounterStore } from './models/useCounterStore';
 import './styles/global.scss';
+const counterStore = useCounterStore();
 </script>
 
 <template>
   <header>
     <div>王明乔网站</div>
+    <button @click="counterStore.increment">
+      添加{{ counterStore.doubleCount }}
+    </button>
   </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
