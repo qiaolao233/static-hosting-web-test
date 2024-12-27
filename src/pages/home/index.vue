28660b9fbde1f1d8c1119dc1754eee958dd06b3d
joejoewang <joejoewang@tungee.com>
2024-12-27 17:43:48 +0800

    feat: 双容器布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>home</div>
+  <div class="home-w">
+    <div class="home-container"></div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home-w {
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--gloabl-backgroud-color);
+}
+
+.home-container {
+  margin: 0 auto;
+  width: 1000px;
+  height: 100%;
+  background-color: pink;
+}
+</style>
