82c9182bf6a89b046445b2ad194de6a009390f16
joejoewang <joejoewang@tungee.com>
2024-12-27 16:51:05 +0800

    追加各种工程化配置

---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: { parser: 'json' },
+    }
+  ],
+};
+
+export default config;
