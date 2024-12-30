57dd1bbf4040b0e1070ee9cd1166a1fe788a3dae
joejoewang <joejoewang@tungee.com>
2024-12-30 22:55:59 +0800

    fix: 处理 改名

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
@@ -22,5 +25,21 @@ export default defineConfig({
     base: './',
     build: {
         cssCodeSplit: false,
+        rollupOptions: {
+            output: {
+                // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+                sanitizeFileName(name) {
+                    const match = DRIVE_LETTER_REGEX.exec(name);
+                    const driveLetter = match ? match[0] : '';
+                    // substr 是被淘汰語法，因此要改 slice
+                    return (
+                        driveLetter +
+                        name
+                            .slice(driveLetter.length)
+                            .replace(INVALID_CHAR_REGEX, '')
+                    );
+                },
+            },
+        },
     },
 });
