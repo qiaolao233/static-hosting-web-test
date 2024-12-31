3e108462f683d101185d97e4c6d20761ac22b1e5
joejoewang <joejoewang@tungee.com>
2024-12-31 23:33:51 +0800

    feat: menu 信息封装

---
+++ apps/wmq-project/src/models/useMenuStore.ts
@@ -0,0 +1,47 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { TWmqMebuItem } from '@wmq/wmq-ui';
+
+export const useMenuStore = defineStore('menuStore', () => {
+    const menuList = ref<TWmqMebuItem[]>([
+        {
+            label: '仙尊语录',
+            value: 'icon-shouye',
+        },
+        {
+            label: '我的游戏',
+            value: 'my-game',
+            children: [
+                {
+                    label: '游戏1',
+                    value: 'game-1',
+                },
+                {
+                    label: '游戏2',
+                    value: 'game-2',
+                },
+                {
+                    label: '游戏3',
+                    value: 'game-3',
+                },
+            ],
+        },
+    ]);
+
+    const menuClick = (ids: string[]) => {
+        let findMenuItem: TWmqMebuItem | undefined = void 0;
+        while (ids.length > 0) {
+            const id = ids.shift();
+            if (id) {
+                findMenuItem = menuList.value.find((item) => item.value === id);
+                if (!findMenuItem) break;
+            }
+        }
+
+        if (findMenuItem && findMenuItem.children) {
+            findMenuItem.isOpen = !findMenuItem.isOpen;
+        }
+    };
+
+    return { menuList, menuClick };
+});
